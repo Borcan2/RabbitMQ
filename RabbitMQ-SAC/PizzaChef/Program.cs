1dd57f9a64ef4601af01b7000e799feec204969d
Saif Mohamed <iti.202020002@thebes.edu.eg>
2024-09-14 23:25:43 +0300

    Add files via upload

---
+++ RabbitMQ-SAC/PizzaChef/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SendPizzaOrder();
+    }
+
+    static void SendPizzaOrder()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+
+        channel.QueueDeclare(queue: "pizza_orders",
+                             durable: true,  
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: new Dictionary<string, object> { { "x-queue-type", "quorum" } });
+
+        string message = "Order: Pizza Margherita";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: "",
+                             routingKey: "pizza_orders",
+                             basicProperties: null,
+                             body: body);
+
+        Console.WriteLine("Sent: {0}", message);
+    }
+}
---
+++ RabbitMQ-SAC/PizzaDelivery/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ReceivePizzaOrder();
+    }
+
+    static void ReceivePizzaOrder()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "pizza_orders",
+                             durable: true,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: new Dictionary<string, object> { { "x-queue-type", "quorum" } });
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine("Received: {0}", message);
+        };
+
+        channel.BasicConsume(queue: "pizza_orders",
+                             autoAck: true,
+                             consumer: consumer);
+
+        Console.WriteLine("Waiting for messages. Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
